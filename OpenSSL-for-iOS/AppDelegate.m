8411473cf7f936968d930abbfc95fa010a458d27
heyunpeng <heyunpeng@17220103165.local>
2016-02-23 16:21:22 +0800

    first commit

---
+++ OpenSSL-for-iOS/AppDelegate.m
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.m
+//  OpenSSL-for-iOS
+//
+//  Created by Felix Schulze on 04.12.2010.
+//  Updated by Schulze Felix on 01.04.12.
+//  Copyright (c) 2012 Felix Schulze . All rights reserved.
+//  Web: http://www.felixschulze.de
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+@synthesize viewController = _viewController;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+@end
